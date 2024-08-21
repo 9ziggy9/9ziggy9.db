3ee2cf09f96c436dc0455a12406c9a2ecdfa620e
9ziggy9 <davidarogers@protonmail.com>
2024-08-20 20:08:23 -0700

    jwt amendments

--- main.go
+++ main.go
@@ -70,11 +70,8 @@ func init() {
 func main() {
 	db_conn_str := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-    os.Getenv("DB_HOST"),
-    os.Getenv("DB_PORT"),
-    os.Getenv("DB_USER"),
-    os.Getenv("DB_PASS"),
-    os.Getenv("DB_NAME"));
+    os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
+    os.Getenv("DB_PASS"), os.Getenv("DB_NAME"));
 
 	db, err := sql.Open("postgres", db_conn_str);
 	if err != nil { srv.Log(srv.ERROR, "%s\n", err); }
